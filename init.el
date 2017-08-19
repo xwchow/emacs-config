80b2283fa93da8803ae67e3951b2e2be41c39f9e
Xin Wei Chow <chowxinwei@gmail.com>
2017-08-19 17:07:04 +1000

    Add ggtags for c++-mode

--- init.el
+++ init.el
@@ -9,8 +9,4 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(setq-default org-src-fontify-natively t)
-(setq-default org-src-preserve-indentation t)
-(setq-default org-src-tab-acts-natively t)
-
-(org-babel-load-file (expand-file-name "/home/xw/.emacs.d/my-init.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/my-init.org"))
